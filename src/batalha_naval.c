fe90e2aba9b40c4d371246a977882df56a46d9ae
H. Nunes <h4mnsoft@gmail.com>
2017-10-07 13:48:30 -0300

    resolvido problema da concatenação do prepara

--- src/batalha_naval.c
+++ src/batalha_naval.c
@@ -14,6 +14,7 @@
 struct embarcacoes
 {
     char nome[15];
+    int colocados[2];
     int tamanho;
     int quantidade;
 };
@@ -29,7 +30,9 @@ struct jogadores
 {
     int jogador_id;
     char nome[15];
-    int blocos_ocupados;
+    int naviosDisponiveis;
+    int blocosOcupados;
+    int blocosRestantes;
 };
 
 struct tabuleiro
@@ -87,22 +90,21 @@ int batalha_naval()
             definirCampos();
         }
     }
-
     return 1;
 }
 
 /*
 * Função desenharTela
 */
-void desenharTela (int tela, int posicao)
+void desenharTela (int tela, int posicao, int instrucao NULL)
 {
-    char linha[5][20];
-    char sTamanho;
+    //int i;
     char *linha_1;
     char *linha_2;
     char *linha_3;
     char *linha_4;
     char *linha_5;
+
     // code
     switch (tela)
     {
@@ -213,13 +215,6 @@ void desenharTela (int tela, int posicao)
         printf("===============================================================================\n");
         break;
     case 3: // prepara Navios
-        strcpy(linha[0], navios[0].nome);
-        strcat(linha[0], " (");
-        itoa((navios[0].tamanho), sTamanho, 10);
-        //printf("DEBUG: Chegou aqui!");
-        strcat(linha[0], sTamanho);
-        strcat(linha[0], " blocos)");
-
         system("cls");
         printf("BATALHA NAVAL - Preparação                                 Blocos restantes: 30\n");
         printf("=============================================|=================================\n");
@@ -228,23 +223,32 @@ void desenharTela (int tela, int posicao)
         printf("0 |<+>|   |   |   |   |   |   |   |   |   |  |  [%s]\n", jogadorAtual.nome);
         printf("  -----------------------------------------  |                                 \n");
         printf("1 |   |   |   |   |   |   |   |   |   |   |  |  Navios (tamanho):              \n");
-        printf("  -----------------------------------------  |  1. [%s]\n", linha[0]);
-        printf("2 |   |   |   |   |   |   |   |   |   |   |  |  2. Torpedeiro (3 blocos)       \n");
-        printf("  -----------------------------------------  |  3. Navio-tanque (4 blocos)     \n");
-        printf("3 |   |   |   |   |   |   |   |   |   |   |  |  4. Porta-avião (5 blocos)      \n");
+        printf("  -----------------------------------------  |  1. %s (%d blocos)\n", navios[0].nome, navios[0].colocados[jogadorAtual.jogador_id]);
+        printf("2 |   |   |   |   |   |   |   |   |   |   |  |  2. %s (%d blocos)\n", navios[1].nome, navios[1].colocados[jogadorAtual.jogador_id]);
+        printf("  -----------------------------------------  |  3. %s (%d blocos)\n", navios[2].nome, navios[2].colocados[jogadorAtual.jogador_id]);
+        printf("3 |   |   |   |   |   |   |   |   |   |   |  |  4. %s (%d blocos)\n", navios[3].nome, navios[3].colocados[jogadorAtual.jogador_id]);
         printf("  -----------------------------------------  |                                 \n");
-        printf("4 |   |   |   |   |   |   |   |   |   |   |  |  Blocos ocupados:               \n");
-        printf("  -----------------------------------------  |  [%02d] blocos\n", jogadorAtual.blocos_ocupados);
+        printf("4 |   |   |   |   |   |   |   |   |   |   |  |  Navios disponíveis:            \n");
+        printf("  -----------------------------------------  |  %02d navios disponíveis\n", jogadorAtual.naviosDisponiveis);
         printf("5 |   |   |   |   |   |   |   |   |   |   |  |                                 \n");
-        printf("  -----------------------------------------  |                                 \n");
-        printf("6 |   |   |   |   |   |   |   |   |   |   |  |                                 \n");
+        printf("  -----------------------------------------  |  Blocos restantes:              \n");
+        printf("6 |   |   |   |   |   |   |   |   |   |   |  |  %02d blocos restantes\n", jogadorAtual.blocosRestantes);
         printf("  -----------------------------------------  |                                 \n");
         printf("7 |   |   |   |   |   |   |   |   |   |   |  |                                 \n");
         printf("  -----------------------------------------  |                                 \n");
         printf("8 |   |   |   |   |   |   |   |   |   |   |  |                                 \n");
         printf("  -----------------------------------------  |                                 \n");
-        printf("9 |   |   |   |   |   |   |   |   |   |   |  |                                 \n");
-        printf("  -----------------------------------------  | ");
+        switch ()
+        {
+        case :
+            break;
+
+        default:
+            break;
+        }
+
+        printf("9 |   |   |   |   |   |   |   |   |   |   |  |  Digite o Navio a ser colocado: \n");
+        printf("  -----------------------------------------  |  [1-4]>");
         break;
     case 4: // quadro
         break;
@@ -304,7 +308,16 @@ int capturarMenu ()
 
 int definirCampos()
 {
-    // criar matriz com os navios disponíveis
+    int i;
+    int navioTamanho[5];
+    int navioQuantidade[5];
+    int naviosBlocosColocados[5];
+    int naviosTotal = 0;
+    int naviosDisponiveis = 0;
+    int blocosTotal = 0;
+    int blocosRestantes = 0;
+
+    // Prenchimento da matriz com os navios disponíveis
     strcpy(navios[0].nome, "Submarino");
         navios[0].tamanho = 2;
         navios[0].quantidade = 4;
@@ -314,18 +327,28 @@ int definirCampos()
     strcpy(navios[2].nome, "Navio-Tanque");
         navios[2].tamanho = 4;
         navios[2].quantidade = 2;
-    strcpy(navios[2].nome, "Porta-Avião");
-        navios[2].tamanho = 5;
-        navios[2].quantidade = 1;
+    strcpy(navios[3].nome, "Porta-Avião");
+        navios[3].tamanho = 5;
+        navios[3].quantidade = 1;
 
     // Laço para os 2 (dois) jogadores
     for (jogadorAtual.jogador_id = 1; jogadorAtual.jogador_id <= 2; jogadorAtual.jogador_id++) {
+        //Coleta nome do jogador
         desenharTela(2, 0);
-        //fgets(jogadorAtual.nome, 15, stdin);
         scanf("%s", jogadorAtual.nome);
+
+        //Coleta posições dos navios
+        for (i=0; i<5; i++) {
+            navios[i].colocados[jogadorAtual.jogador_id] = navios[i].tamanho;
+            naviosTotal = naviosTotal + navios[i].quantidade;
+            blocosTotal = blocosTotal + (navios[i].quantidade * navios[i].tamanho);
+        }
+        naviosDisponiveis = naviosTotal;
+        blocosRestantes = blocosTotal;
+
         desenharTela(3, 0);
 
         getch();
     }
-
+    return 0;
 }
